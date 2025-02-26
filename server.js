be03919f0abbe25f2649bb588e7b5a8113c289a8
yeabtsega45 <yabtsega2022@gmail.com>
2025-02-26 11:25:01 +0300

    create server file

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const connectDb = require("./config/connectDb");
+const authController = require("./controllers/authController");
+const workController = require("./controllers/workController");
+
+const app = express();
+connectDb();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use("/images", express.static("public/images"));
+
+app.use("/auth", authController);
+app.use("/work", workController);
+
+app.listen(process.env.PORT, () =>
+  console.log("server has been started successfully!")
+);
